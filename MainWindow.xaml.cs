18c77b828f7e2171349bd54d56c48c385c52340f
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2020-10-16 17:09:13 +0200

    Completed of the code responsible for adding and editing accounts

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace BillManagerWPF
         private readonly IBillsService billsService;
         private readonly IUsersService usersService;
         List<Bill> billsList = new List<Bill>();
+        Bill updateBill;
         ContextMenu contextMenu = null;
         TextBox januaryTextBox, februaryTextBox, marchTextBox, aprilTextBox, mayTextBox,
                 juneTextBox, julyTextBox, augustTextBox, septemberTextBox, octoberTextBox, novemberTextBox, decemberTextBox, nameTextBox;
@@ -200,7 +201,137 @@ namespace BillManagerWPF
         }
         private void appDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            contextMenu = new ContextMenu();
 
+            foreach(Bill item in appDataGrid.SelectedItems)
+            {
+                updateBill = new Bill
+                {
+                    Id = item.Id,
+                    April = item.April,
+                    August = item.August,
+                    BillName = item.BillName,
+                    BillsYear = item.BillsYear,
+                    December = item.December,
+                    February = item.February,
+                    January = item.January,
+                    July = item.July,
+                    June = item.June,
+                    March = item.March,
+                    May = item.May,
+                    November = item.November,
+                    October = item.October,
+                    September = item.September
+                };
+            }
+
+            textId = new TextBlock();
+            textId.Text = updateBill.Id.ToString();
+            contextMenu.Items.Add(textId);
+
+            nameTextBox = new TextBox();
+            nameTextBox.Text = updateBill.BillName.ToString();
+            contextMenu.Items.Add("Nazwa");
+
+            januaryTextBox = new TextBox();
+            januaryTextBox.Text = updateBill.January.ToString();
+            contextMenu.Items.Add("Styczeń");
+            contextMenu.Items.Add(januaryTextBox);
+
+            februaryTextBox = new TextBox();
+            februaryTextBox.Text = updateBill.February.ToString();
+            contextMenu.Items.Add("Luty");
+            contextMenu.Items.Add(februaryTextBox);
+
+            marchTextBox = new TextBox();
+            marchTextBox.Text = updateBill.March.ToString();
+            contextMenu.Items.Add("Marzec");
+            contextMenu.Items.Add(marchTextBox);
+
+            aprilTextBox = new TextBox();
+            aprilTextBox.Text = updateBill.April.ToString();
+            contextMenu.Items.Add("Kwiecień");
+            contextMenu.Items.Add(aprilTextBox);
+
+            mayTextBox = new TextBox();
+            mayTextBox.Text = updateBill.May.ToString();
+            contextMenu.Items.Add("Maj");
+            contextMenu.Items.Add(mayTextBox);
+
+            juneTextBox = new TextBox();
+            juneTextBox.Text = updateBill.June.ToString();
+            contextMenu.Items.Add("Czerwiec");
+            contextMenu.Items.Add(juneTextBox);
+
+            julyTextBox = new TextBox();
+            julyTextBox.Text = updateBill.July.ToString();
+            contextMenu.Items.Add("Lipiec");
+            contextMenu.Items.Add(julyTextBox);
+
+            augustTextBox = new TextBox();
+            augustTextBox.Text = updateBill.August.ToString();
+            contextMenu.Items.Add("Sierpień");
+            contextMenu.Items.Add(augustTextBox);
+
+            septemberTextBox = new TextBox();
+            septemberTextBox.Text = updateBill.September.ToString();
+            contextMenu.Items.Add("Wrzesień");
+            contextMenu.Items.Add(septemberTextBox);
+
+            octoberTextBox = new TextBox();
+            octoberTextBox.Text = updateBill.October.ToString();
+            contextMenu.Items.Add("Październik");
+            contextMenu.Items.Add(octoberTextBox);
+
+            novemberTextBox = new TextBox();
+            novemberTextBox.Text = updateBill.November.ToString();
+            contextMenu.Items.Add("Listopad");
+            contextMenu.Items.Add(novemberTextBox);
+
+            decemberTextBox = new TextBox();
+            decemberTextBox.Text = updateBill.December.ToString();
+            contextMenu.Items.Add("Grudzień");
+            contextMenu.Items.Add(decemberTextBox);
+
+            Button buttonSave = new Button();
+            buttonSave.Content = "Zapisz";
+            contextMenu.Items.Add(buttonSave);
+            buttonSave.Click += new RoutedEventHandler(buttonSave_Click);
+        }
+
+        void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            Bill bill = new Bill();
+
+            foreach(Bill item in billsList)
+            {
+                if(item.Id == Convert.ToInt32(textId.Text))
+                {
+                    item.April = Double.Parse(aprilTextBox.Text == "" ? "-" : aprilTextBox.Text);
+                    item.August = Double.Parse(augustTextBox.Text == "" ? "-" : augustTextBox.Text);
+                    item.BillName = nameTextBox.Text;
+                    item.December = Double.Parse(decemberTextBox.Text == "" ? "-" : decemberTextBox.Text);
+                    item.February = Double.Parse(februaryTextBox.Text == "" ? "-" : februaryTextBox.Text);
+                    item.January = Double.Parse(januaryTextBox.Text == "" ? "-" : januaryTextBox.Text);
+                    item.July = Double.Parse(julyTextBox.Text == "" ? "-" : julyTextBox.Text);
+                    item.June = Double.Parse(juneTextBox.Text == "" ? "-" : juneTextBox.Text);
+                    item.March = Double.Parse(marchTextBox.Text == "" ? "-" : marchTextBox.Text);
+                    item.May = Double.Parse(mayTextBox.Text == "" ? "-" : mayTextBox.Text);
+                    item.November = Double.Parse(novemberTextBox.Text == "" ? "-" : novemberTextBox.Text);
+                    item.October = Double.Parse(octoberTextBox.Text == "" ? "-" : octoberTextBox.Text);
+                    item.September = Double.Parse(septemberTextBox.Text == "" ? "-" : septemberTextBox.Text);
+
+                    billsService.Edit(item);
+                }
+            }
+
+            appDataGrid.SelectionChanged -= new SelectionChangedEventHandler(appDataGrid_SelectionChanged);
+            appDataGrid.ItemsSource = null;
+            appDataGrid.ItemsSource = billsList;
+            appDataGrid.SelectedIndex = -1;
+
+            contextMenu.IsOpen = false;
+            appDataGrid.SelectionChanged += new SelectionChangedEventHandler(appDataGrid_SelectionChanged);
         }
     }
 }
